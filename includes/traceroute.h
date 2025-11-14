06c9a9dff51d30b960f9421795eacb2f3730934b
Ustrif <39741608+Ustrif@users.noreply.github.com>
2025-11-14 10:22:21 +0300

    .h added.

---
+++ includes/traceroute.h
@@ -0,0 +1,13 @@
+#ifndef __traceroute__
+#define __traceroute__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+
+
+
+#endif // __traceroute__
