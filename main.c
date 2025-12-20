6e340795d29f57fb58f3bbd5edcb51ff4a7e09ec
Ustrif <39741608+Ustrif@users.noreply.github.com>
2025-12-20 14:38:50 +0300

    main template is edited.

--- main.c
+++ main.c
@@ -1,18 +1,43 @@
 #include "traceroute.h"
 
+static int	run(t_args *args, in_addr_t ip)
+{
+	int	result;
+
+	result = 0;
+	switch (args->protocol)
+	{
+		case ICMP:
+			result = icmp(args, ip);
+			break ;
+		default:
+			break;
+	}
+	if (result < 0)
+		return (1);
+	else if (result)
+	{
+		printf("\n* had not reached.\n");
+		return (1);
+	}
+
+	printf("\n* Arrived! *\n");
+	return (0);
+}
+
 int	main(int argc, char *argv[])
 {
 	t_args		args;
-	t_program	pr;
-	char		*ip;
+	in_addr_t	ip;
 
 	init_t_args(&args);
 	if (parse_args(&args, argc, argv) || check_args(&args))
 		return (1);
-	ip = get_ip_address(&args);
+	ip = get_ip_address(args.host);
 	if (!ip)
 		return (1);
-	init_program(&pr, &args, ip);
-	
-	return (0);
+
+	// INTRO
+	// ekle.
+	return (run(&args, ip));
 }
